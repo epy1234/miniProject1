5576b7f8777f008134503c03818fd3a206fb5a53
Ephraim Eisikovits <87619902+epy1234@users.noreply.github.com>
2021-10-14 00:44:46 +0300

    Update check_words.py

--- check_words.py
+++ check_words.py
@@ -7,15 +7,4 @@ def longest_words():
     return max_len
 
 
-""" def word_count():
-    Count = 0
 
-    with open(r"C:\Users\epy12\miniProject1\file.txt") as f:
-        data= f.readlines()
-        for line in data:
-            words = line.split()
-            for word in words:
-                if word == "Epy":
-                    Count+=1
-    return Count
- """
